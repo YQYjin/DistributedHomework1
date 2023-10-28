a28ab63bdc72265c36a492fd57c746355b4bd896
YQYjin <yqyjinhuang@163.com>
2023-10-28 12:05:32 +0800

    分布式大作业1

--- src/main/java/mywebproject/dataBaseComponent/UserDB.java
+++ src/main/java/mywebproject/dataBaseComponent/UserDB.java
@@ -5,71 +5,71 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class UserDB{
+public class UserDB {
     // 负责用户的 增删查改
-    static Connection conn = DBServer.getConnection ();
-    static Statement statement = null;
+    static Connection conn = DBServer.getConnection();
+    public static Statement statement = null;
 
     static {
         try {
-            statement = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);;
+            statement = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            ;
             String sqlStr = "use bloginfos";
             statement.executeUpdate(sqlStr);
         } catch (SQLException e) {
-            throw new RuntimeException (e);
+            throw new RuntimeException(e);
         }
     }
 
-    public static void addUser(String id, String userName, String pwd, String isOnline){
+    public static void addUser(String id, String userName, String pwd, String isOnline) {
         String sql = "insert into user values('" + id + "','" + userName + "','" + pwd + "','" + isOnline + "')";
         try {
-            statement.executeUpdate (sql);
+            statement.executeUpdate(sql);
         } catch (SQLException e) {
-            throw new RuntimeException (e);
+            throw new RuntimeException(e);
         }
     }
 
     // 查询用户是否存在
-    public static boolean isUserExist(String userName){
+    public static boolean isUserExist(String userName) {
         String sql = "select * from user where userName = '" + userName + "'";
         try {
             //若有结果则next返回true
-            return statement.executeQuery (sql).next ();
+            return statement.executeQuery(sql).next();
         } catch (SQLException e) {
-            throw new RuntimeException (e);
+            throw new RuntimeException(e);
         }
     }
 
     // 查询用户密码是否正确
-    public static boolean isPwdCorrect(String userName,String pwd){
+    public static boolean isPwdCorrect(String userName, String pwd) {
         String sql = "select * from user where userName = '" + userName + "' and password = '" + pwd + "'";
         try {
-            return statement.executeQuery (sql).next ();
+            return statement.executeQuery(sql).next();
         } catch (SQLException e) {
-            throw new RuntimeException (e);
+            throw new RuntimeException(e);
         }
     }
 
     // 查询用户是否在线
-    public static boolean isUserOnline(String userName){
+    public static boolean isUserOnline(String userName) {
         String sql = "select * from user where userName = '" + userName + "' and isOnline = 'true'";
         try {
-            return statement.executeQuery (sql).next ();
+            return statement.executeQuery(sql).next();
         } catch (SQLException e) {
-            throw new RuntimeException (e);
+            throw new RuntimeException(e);
         }
     }
 
     // 修改用户在线状态
-    public static void changeUserOnline(String userName,String isOnline){
+    public static void changeUserOnline(String userName, String isOnline) {
         String sql = "update user set isOnline = '" + isOnline + "' where userName = '" + userName + "'";
         try {
-            statement.executeUpdate (sql);
+            statement.executeUpdate(sql);
         } catch (SQLException e) {
-            throw new RuntimeException (e);
+            throw new RuntimeException(e);
         }
     }
-
 
 
 }
--- src/main/java/mywebproject/servletComponent/BlogServlet.java
+++ src/main/java/mywebproject/servletComponent/BlogServlet.java
@@ -1,45 +1,91 @@
 package mywebproject.servletComponent;
 
 import com.google.gson.Gson;
-import mywebproject.UserService;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import mywebproject.UserService;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+
+import static mywebproject.dataBaseComponent.UserDB.statement;
 
 @WebServlet(name = "blogServlet", value = "/blog")
 public class BlogServlet extends HttpServlet {
     private String message;
+
     public void init() {
         UserService.init();
     }
+
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         ArrayList<BlogPost> blogList = new ArrayList<>();
         //模拟博客信息
-        for(int i=0;i<20;++i){
+        String query = "SELECT title, content, post_time FROM blogs";
+        ResultSet resultSet = null;
+        try {
+            resultSet = statement.executeQuery(query);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Iterate through the result set and create BlogPost objects
+        while (true) {
+            try {
+                if (!resultSet.next()) break;
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            String title = null;
+            try {
+                title = resultSet.getString("title");
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            String content = null;
+            try {
+                content = resultSet.getString("content");
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            String date = null;
+            try {
+                date = resultSet.getString("post_time");
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            BlogPost blogPost = new BlogPost(title, content, date);
+            blogList.add(blogPost);
+        }
+        for (BlogPost blogPost : blogList) {
+            System.out.println(blogPost.title);
+        }
+        /*for(int i=0;i<20;++i){
             BlogPost blogPost = new BlogPost(
                     "博客标题"+i,
                     "博客内容",
                     "上传时间"
             );
             blogList.add(blogPost);
-        }
+        }*/
         //使用Gson转换
         Gson gson = new Gson();
-        String json= gson.toJson(blogList);
+        String json = gson.toJson(blogList);
         //设置响应格式
         response.setContentType("application/json");
         //发送
-        PrintWriter writer=response.getWriter();
+        PrintWriter writer = response.getWriter();
         writer.write(json);
         writer.close();
     }
-    public void destroy () {
+
+    public void destroy() {
 
     }
 }
